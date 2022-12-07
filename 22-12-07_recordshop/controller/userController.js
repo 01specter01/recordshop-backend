c370670d6cdfff89128b2b11a003d87dd92a48b1
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-07 15:39:35 +0100

    h

--- 22-12-07_recordshop/controller/userController.js
+++ 22-12-07_recordshop/controller/userController.js
@@ -1,11 +1,11 @@
 import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 
-const db = new Low(new JSONFile("../data/db.json"));
+const db = new Low(new JSONFile("data/db.json"));
 
 export const getAllUser = async (req, res) => {
     await db.read();
-    res.json(db);
+    res.json(db.data);
 };
 
 export const getUser = async (req, res) => {
