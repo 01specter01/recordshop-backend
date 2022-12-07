36ae8e74ee5f48591055f49b0b4979ccfe132eb1
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-07 14:19:40 +0100

    r

--- 22-12-07_recordshop/main.js
+++ 22-12-07_recordshop/main.js
@@ -1,5 +1,4 @@
 import express from "express";
 const app = express();
 
-app.listen(1234,() => console.log("listening to port 1234"))
-
+app.listen(1234, () => console.log("listening to port 1234"));
